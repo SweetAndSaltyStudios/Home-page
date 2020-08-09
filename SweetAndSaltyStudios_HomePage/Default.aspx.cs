8e943dca9a6dbadcfba9d786292b71e11efef4fc
Mathias Mustonen <mathias.mustonen@gmail.com>
2020-08-10 00:07:28 +0300

    Project cleaning

---
+++ SweetAndSaltyStudios_HomePage/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SweetAndSaltyStudios_HomePage
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TextBox1.Text = "0";
+            }   
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int clickCount = Convert.ToInt32(TextBox1.Text) + 1;
+            TextBox1.Text = clickCount.ToString();
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SweetAndSaltyStudios_HomePage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SweetAndSaltyStudios_HomePage.Global" Language="C#" %>
